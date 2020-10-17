c38ed341473c92b5dc2fe6d9df9c1c85973ab7a3
JoseLRM <joselrm890@gmail.com>
2020-10-17 18:21:14 +0200

    Properly Windows.h include

--- SilverEngine/include/platform/windows_impl.h
+++ SilverEngine/include/platform/windows_impl.h
@@ -1,7 +1,39 @@
 #pragma once
 
-#define UNICODE
 #define WIN32_LEAN_AND_MEAN
+#define NOGDICAPMASKS
+#define NOSYSMETRICS
+#define NOMENUS
+#define NOICONS
+#define NOSYSCOMMANDS
+#define NORASTEROPS
+#define OEMRESOURCE
+#define NOATOM
+#define NOCLIPBOARD
+#define NOCOLOR
+#define NOCTLMGR
+#define NODRAWTEXT
+#define NOKERNEL
+//#define NONLS			(MultiByteToWideChar)
+#define NOMEMMGR
+#define NOMETAFILE
+#define NOOPENFILE
+#define NOSCROLL
+#define NOSERVICE
+#define NOSOUND
+#define NOTEXTMETRIC
+#define NOWH
+#define NOCOMM
+#define NOKANJI
+#define NOHELP
+#define NOPROFILER
+#define NODEFERWINDOWPOS
+#define NOMCX
+#define NORPC
+#define NOPROXYSTUB
+#define NOIMAGE
+#define NOTAPE
+#define UNICODE
 #define NOMINMAX
 
 #include <Windows.h>
