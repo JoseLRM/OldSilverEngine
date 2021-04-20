c008dd2407d546925637190b102b19d4829e2992
JoseLRM <JoseLRM890@gmail.com>
2021-04-20 12:02:41 +0200

    fixing default game code

--- Game/default_code.cpp
+++ Game/default_code.cpp
@@ -6,18 +6,17 @@ struct GameMemory {
     Entity foo;
 };
 
-GameMemory& get_game_memory()
-{
-    return *reinterpret_cast<GameMemory*>(engine.game_memory);
-}
+GameMemory* game = nullptr;
 
 SV_USER bool user_initialize(bool init)
 {
     if (init) {
-	engine.game_memory = allocate_memory(sizeof(GameMemory));
+	engine.game_memory = SV_ALLOCATE_STRUCT(GameMemory);
     
 	set_active_scene("Test");
     }
+
+    game = reinterpret_cast<GameMemory*>(engine.game_memory);
     
     return true;
 }
@@ -25,18 +24,16 @@ SV_USER bool user_initialize(bool init)
 SV_USER bool user_close(bool close)
 {
     if (close)
-	free_memory(engine.game_memory);
+	SV_FREE_STRUCT(GameMemory, game);
     
     return true;
 }
 
 SV_USER bool user_initialize_scene(Scene* scene, Archive* parchive)
-{
-    GameMemory& m = get_game_memory();
-    
+{    
     if (parchive) {
 	Archive& archive = *parchive;
-	archive >> m.foo;
+	archive >> game->foo;
 	return true;
     }
 
@@ -46,39 +43,30 @@ SV_USER bool user_initialize_scene(Scene* scene, Archive* parchive)
     add_component<CameraComponent>(scene, scene->main_camera);
 
     // Create some entity
-    m.foo = create_entity(scene, SV_ENTITY_NULL, "Foo");
-    SpriteComponent* spr = add_component<SpriteComponent>(scene, m.foo);
+    game->foo = create_entity(scene, SV_ENTITY_NULL, "Foo");
+    SpriteComponent* spr = add_component<SpriteComponent>(scene, game->foo);
     spr->color = Color::Salmon();
-    
-    BodyComponent* body = add_component<BodyComponent>(scene, m.foo);
-    body->body_type = BodyType_Dynamic;
 
     return true;
 }
 
 SV_USER void user_update()
 {
-    GameMemory& m = get_game_memory();
+    if (entity_exist(engine.scene, game->foo)) {
 
-    if (entity_exist(engine.scene, m.foo)) {
-
-	Transform trans = get_entity_transform(engine.scene, m.foo);
-	v2_f32 pos = trans.getLocalPosition().getVec2();
+	v2_f32& pos = *get_entity_position2D_ptr(engine.scene, game->foo);
 
 	f64 t = timer_now();
 
 	pos.x = f32(cos(t)) * 4.f;
 	pos.y = f32(sin(t)) * 2.f;
-
-	trans.setPosition(pos.getVec3());
     }
 }
 
 SV_USER bool user_serialize_scene(Scene* scene, Archive* parchive)
 {
-    GameMemory& m = get_game_memory();
     Archive& archive = *parchive;
-    archive << m.foo;
+    archive << game->foo;
     return true;
 }
 
